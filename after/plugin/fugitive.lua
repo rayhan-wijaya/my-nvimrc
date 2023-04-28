b09ff4f58180496154285b16aec7c850e8700c78
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-04-28 16:45:02 +0700

    Added keymaps for fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,19 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+vim.keymap.set("n", "<leader>ga", function ()
+  local filePath = vim.fn.expand("%");
+  vim.cmd("G add " .. filePath)
+
+  print("Added: " .. filePath)
+end)
+
+vim.keymap.set("n", "<leader>gu", function ()
+  local filePath = vim.fn.expand("%");
+  vim.cmd("G restore --staged " .. filePath)
+
+  print("Removed: " .. filePath)
+end)
+
+vim.keymap.set("n", "<leader>gc", function ()
+  vim.cmd("G commit")
+end)

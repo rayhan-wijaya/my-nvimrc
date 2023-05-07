501c08b67c45164ccfce4b3ce587779061d56876
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-07 22:07:28 +0700

    Fixed git pull mapping

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -35,5 +35,5 @@ vim.keymap.set("n", "<leader>gc", ":G commit<CR>")
 vim.keymap.set("n", "<leader>gfc", ":G commit --amend<CR>")
 vim.keymap.set("n", "<leader>gd", ":G diff<CR>")
 vim.keymap.set("n", "<leader>gpus", ":G push<CR>")
-vim.keymap.set("n", "<leader>gpul", ":G push<CR>")
+vim.keymap.set("n", "<leader>gpul", ":G pull<CR>")
 

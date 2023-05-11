52f38996f429ffffaeb4d2c260963f22e79beed6
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-11 19:59:28 +0700

    Created mapping for printing pdf (latex) files

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -89,3 +89,23 @@ vim.keymap.set("n", "<leader>lpd", function ()
   vim.cmd(command)
 end)
 
+vim.keymap.set("n", "<leader>lprint", function ()
+  local filePath = vim.fn.expand("%:p")
+
+  local fileExtensionRegex = "%.([^%.]+)$"
+  local fileExtension = string.sub(
+    filePath,
+    string.find(filePath, fileExtensionRegex)
+  )
+
+  if fileExtension ~= ".tex" then
+    return
+  end
+
+  local fileName = string.match(filePath, ".+[\\/](.-)%.%w+$")
+  local directoryPath = string.match(filePath, "^(.*\\)[^\\]+$")
+
+  local command = string.format("! pdftoprinter %sdist\\%s.pdf", directoryPath, fileName)
+  vim.cmd(command)
+end)
+

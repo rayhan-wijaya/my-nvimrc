edf0581f35f4fd94bac056af44b112a3a03dace7
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-11 19:27:19 +0700

    Created mapping for viewing pdf files with mupdf

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -69,3 +69,22 @@ end)
 vim.keymap.set("n", "<leader>y", "\"*y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 
+vim.keymap.set("n", "<leader>pdf", function ()
+  local filePath = vim.fn.expand("%:p")
+
+  local fileExtensionRegex = "%.([^%.]+)$"
+  local fileExtension = string.sub(
+    filePath,
+    string.find(filePath, fileExtensionRegex)
+  )
+
+  if fileExtension ~= ".tex" then
+    return
+  end
+
+  local fileName = string.match(filePath, ".+[\\/](.-)%.%w+$")
+  local directoryPath = string.match(filePath, "^(.*\\)[^\\]+$")
+
+  local command = string.format("!mupdf %sdist\\%s.pdf", directoryPath, fileName)
+  vim.cmd(command)
+end)

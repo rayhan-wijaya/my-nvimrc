7e1a0f10f87fec846d7e61df9c831a0e7a59964a
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-26 17:49:24 +0700

    Configured Sign icons in lsp-zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,4 +7,11 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+lsp.set_sign_icons({
+  error = '×',
+  warn = '▲',
+  hint = '?',
+  info = '»',
+})
+
 lsp.setup()

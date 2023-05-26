85c494b99389b92979ae44997f37e60845370eea
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-26 22:19:34 +0700

    Changed relative line number color

--- lua/rayhan/colorscheme.lua
+++ lua/rayhan/colorscheme.lua
@@ -1,5 +1,8 @@
 vim.cmd.colorscheme("nord")
 
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#499FD1', bold=true })
-vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#CD4074', bold=true })
+local number_color = "#ffffff"
+local relative_number_color = "#5C6780"
+
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg=relative_number_color, bold=true })
+vim.api.nvim_set_hl(0, 'LineNr', { fg=number_color, bold=true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg=relative_number_color, bold=true })

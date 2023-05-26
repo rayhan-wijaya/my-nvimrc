61b8cd3582ca53cea8de3c5368b2c67eca114494
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-26 21:04:55 +0700

    Set colorscheme to nord

--- lua/rayhan/colorscheme.lua
+++ lua/rayhan/colorscheme.lua
@@ -1,4 +1,4 @@
-require("onenord").setup()
+vim.cmd.colorscheme("nord")
 
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#499FD1', bold=true })
 vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })

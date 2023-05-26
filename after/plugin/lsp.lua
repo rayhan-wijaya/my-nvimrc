f06c23c8db4344fab60a9fdfb247ce617056d4d8
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-26 21:26:54 +0700

    Changed single quote to double quotes at lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,17 +1,17 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(_, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
 -- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.set_sign_icons({
-  error = '×',
-  warn = '▲',
-  hint = '?',
-  info = '»',
+  error = "×",
+  warn = "▲",
+  hint = "?",
+  info = "»",
 })
 
 lsp.setup()

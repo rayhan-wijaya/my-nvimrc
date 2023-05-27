d31ee5831ab5de07985ef192aa1ef9659fa654be
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-27 23:47:41 +0700

    Moved coloring line numbers to a function

--- lua/rayhan/colorscheme.lua
+++ lua/rayhan/colorscheme.lua
@@ -1,12 +1,15 @@
+local color_line_numbers = function ()
+  local number_color = "#ffffff"
+  local relative_number_color = "#5C6780"
+
+  vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function ()
+      vim.api.nvim_set_hl(0, 'LineNrAbove', { fg=relative_number_color, bold=true })
+      vim.api.nvim_set_hl(0, 'LineNr', { fg=number_color, bold=true })
+      vim.api.nvim_set_hl(0, 'LineNrBelow', { fg=relative_number_color, bold=true })
+    end
+  })
+end
+
 vim.cmd.colorscheme("kyotonight")
-
-local number_color = "#ffffff"
-local relative_number_color = "#5C6780"
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function ()
-    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg=relative_number_color, bold=true })
-    vim.api.nvim_set_hl(0, 'LineNr', { fg=number_color, bold=true })
-    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg=relative_number_color, bold=true })
-  end
-})
+color_line_numbers()

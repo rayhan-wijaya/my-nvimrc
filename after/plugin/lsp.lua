8dbaa2c64b1a7f0ee2cb0631121e6c4adeaa152f
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-27 10:37:09 +0700

    Configured lsp keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,4 +14,12 @@ lsp.set_sign_icons({
   info = "»",
 })
 
+lsp.on_attach(function (_, bufnr)
+  local opts = { buffer = bufnr, remap = false }
+
+  vim.keymap.set("n", "<leader>rs", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+end)
+
 lsp.setup()

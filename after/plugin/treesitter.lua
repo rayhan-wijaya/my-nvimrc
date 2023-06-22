ce8e7e7c90e9282e8085dff046032d875a998ce2
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-22 20:54:11 +0700

    removed some languages i don't need

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,13 +2,10 @@ require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = {
     "lua",
-    "vim",
     "vimdoc",
-    "query",
     "javascript",
     "tsx",
     "latex",
-    "python",
     "astro"
   },
 

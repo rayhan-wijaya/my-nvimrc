e16910a5930f0e20ebf02a81dc8adf4badb1e7c3
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-22 20:53:23 +0700

    removed highlight

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -11,7 +11,6 @@ require 'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
-    disable = { "rust", "markdown" },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).

8185de23094ac60593ce26daa300a774e692803d
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-23 21:52:53 +0700

    commented compile-latex

--- lua/documents/compile-latex.lua
+++ lua/documents/compile-latex.lua
@@ -1,22 +1,22 @@
-local generateCompileLatexFile = function (outputToDist)
-  local compileLatexFile = function ()
-    local filePath = vim.fn.expand("%:p")
-    local command = string.format("!pdflatex %s", filePath)
+-- local generateCompileLatexFile = function (outputToDist)
+--   local compileLatexFile = function ()
+--     local filePath = vim.fn.expand("%:p")
+--     local command = string.format("!pdflatex %s", filePath)
 
-    if outputToDist then
-      local distDirectoryPath = string.gsub(filePath, "[^/\\]+$", "dist")
-      local outputDirectoryFlag = string.format("--output-directory %s", distDirectoryPath)
+--     if outputToDist then
+--       local distDirectoryPath = string.gsub(filePath, "[^/\\]+$", "dist")
+--       local outputDirectoryFlag = string.format("--output-directory %s", distDirectoryPath)
 
-      command = string.format("%s %s", command, outputDirectoryFlag)
-    end
+--       command = string.format("%s %s", command, outputDirectoryFlag)
+--     end
 
-    vim.cmd(command)
-    print("Ran " .. command)
-  end
+--     vim.cmd(command)
+--     print("Ran " .. command)
+--   end
 
-  return compileLatexFile
-end
+--   return compileLatexFile
+-- end
 
-vim.keymap.set("n", "<leader>ol", generateCompileLatexFile(false))
-vim.keymap.set("n", "<leader>dl", generateCompileLatexFile(true))
+-- vim.keymap.set("n", "<leader>ol", generateCompileLatexFile(false))
+-- vim.keymap.set("n", "<leader>dl", generateCompileLatexFile(true))
 

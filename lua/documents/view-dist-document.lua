ae4c5adf688c20dcf595c9591898939872ecf7c2
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-23 21:52:45 +0700

    commented view-dist-document

--- lua/documents/view-dist-document.lua
+++ lua/documents/view-dist-document.lua
@@ -1,20 +1,20 @@
-vim.keymap.set("n", "<leader>dpo", function ()
-  local filePath = vim.fn.expand("%:p")
+-- vim.keymap.set("n", "<leader>dpo", function ()
+--   local filePath = vim.fn.expand("%:p")
 
-  local fileExtensionRegex = "%.([^%.]+)$"
-  local fileExtension = string.sub(
-    filePath,
-    string.find(filePath, fileExtensionRegex)
-  )
+--   local fileExtensionRegex = "%.([^%.]+)$"
+--   local fileExtension = string.sub(
+--     filePath,
+--     string.find(filePath, fileExtensionRegex)
+--   )
 
-  if fileExtension ~= ".tex" then
-    return
-  end
+--   if fileExtension ~= ".tex" then
+--     return
+--   end
 
-  local fileName = string.match(filePath, ".+[\\/](.-)%.%w+$")
-  local directoryPath = string.match(filePath, "^(.*\\)[^\\]+$")
+--   local fileName = string.match(filePath, ".+[\\/](.-)%.%w+$")
+--   local directoryPath = string.match(filePath, "^(.*\\)[^\\]+$")
 
-  local command = string.format("!mupdf %sdist\\%s.pdf", directoryPath, fileName)
-  vim.cmd(command)
-end)
+--   local command = string.format("!mupdf %sdist\\%s.pdf", directoryPath, fileName)
+--   vim.cmd(command)
+-- end)
 

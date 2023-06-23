ff91745cb194e121ed74789318289568d9f0ffa9
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-23 21:53:31 +0700

    commented old document module requires

--- lua/documents/init.lua
+++ lua/documents/init.lua
@@ -1,4 +1,5 @@
-require("documents.compile-latex")
-require("documents.print-to-dist")
-require("documents.view-dist-document")
 require("documents.view-pdf")
+
+-- require("documents.compile-latex")
+-- require("documents.print-to-dist")
+-- require("documents.view-dist-document")

6b8ca5bd08f8a9134b2a57a873ff9ec79c6498d4
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-27 08:57:55 +0700

    disabled lsp signcolumn icons

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,10 +8,10 @@ end)
 require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.set_sign_icons({
-  error = "×",
-  warn = "▲",
-  hint = "?",
-  info = "»",
+  error = "",
+  warn = "",
+  hint = "",
+  info = "",
 })
 
 lsp.on_attach(function (_, bufnr)

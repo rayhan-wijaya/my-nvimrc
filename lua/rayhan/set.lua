1a34c944f3dc4ed279c43596c3d383b6618f51b8
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-10 21:32:18 +0700

    moved transparency stuff to transparent.lua

--- lua/rayhan/set.lua
+++ lua/rayhan/set.lua
@@ -72,29 +72,3 @@ vim.opt.shortmess = "filnxtToFW"
 vim.g.netrw_banner = 0
 vim.g.netrw_bufsettings = "nu rnu nowrap"
 vim.g.netrw_list_hide = "^\\./$"
-
-local is_transparent = true
-local old_guibg_value = nil
-
-local function update_guibg()
-  local new_guibg = is_transparent and "none" or old_guibg_value
-  vim.cmd("highlight Normal guibg=" .. new_guibg)
-end
-
-vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
-  callback = function ()
-    local highlight_group = vim.api.nvim_exec("highlight Normal", true)
-    local guibg_value = string.match(highlight_group, "guibg=(#(%S+))")
-
-    if old_guibg_value == nil then
-      old_guibg_value = guibg_value
-    end
-
-    update_guibg()
-  end
-})
-
-vim.keymap.set("n", "<leader>tt", function ()
-  is_transparent = not is_transparent
-  update_guibg()
-end)
---
+++ lua/rayhan/transparent.lua
@@ -0,0 +1,25 @@
+local is_transparent = true
+local old_guibg_value = nil
+
+local function update_guibg()
+  local new_guibg = is_transparent and "none" or old_guibg_value
+  vim.cmd("highlight Normal guibg=" .. new_guibg)
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+  callback = function ()
+    local highlight_group = vim.api.nvim_exec("highlight Normal", true)
+    local guibg_value = string.match(highlight_group, "guibg=(#(%S+))")
+
+    if old_guibg_value == nil then
+      old_guibg_value = guibg_value
+    end
+
+    update_guibg()
+  end
+})
+
+vim.keymap.set("n", "<leader>tt", function ()
+  is_transparent = not is_transparent
+  update_guibg()
+end)

ca88b65fd726fd10dc8544bbea2c051a6276308f
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-10 21:23:15 +0700

    a more sophisticated transparency system

--- lua/rayhan/set.lua
+++ lua/rayhan/set.lua
@@ -73,9 +73,28 @@ vim.g.netrw_banner = 0
 vim.g.netrw_bufsettings = "nu rnu nowrap"
 vim.g.netrw_list_hide = "^\\./$"
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
+local is_transparent = true
+local old_guibg_value = nil
+
+local function update_guibg()
+  local new_guibg = is_transparent and "none" or old_guibg_value
+  vim.cmd("highlight Normal guibg=" .. new_guibg)
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
   callback = function ()
-    vim.cmd("highlight Normal guibg=none")
-    vim.cmd("highlight NonText guibg=none")
+    local highlight_group = vim.api.nvim_exec("highlight Normal", true)
+    local guibg_value = string.match(highlight_group, "guibg=(#(%S+))")
+
+    if old_guibg_value == nil then
+      old_guibg_value = guibg_value
+    end
+
+    update_guibg()
   end
 })
+
+vim.keymap.set("n", "<leader>tt", function ()
+  is_transparent = not is_transparent
+  update_guibg()
+end)

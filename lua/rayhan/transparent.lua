e159696859dad420a3bbc0bfc3b506508b3a23ae
the4444 <rayhanwijaya4444@gmail.com>
2023-07-11 08:15:15 +0700

    changed <leader>tt to <leader>te

--- lua/rayhan/transparent.lua
+++ lua/rayhan/transparent.lua
@@ -19,7 +19,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
   end
 })
 
-vim.keymap.set("n", "<leader>tt", function ()
+vim.keymap.set("n", "<leader>te", function ()
   is_transparent = not is_transparent
   update_guibg()
 end)

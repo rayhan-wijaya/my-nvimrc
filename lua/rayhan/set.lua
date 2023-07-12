c2321726b37c61cc04ed91bfb252fa6b2251ba3e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-12 21:48:00 +0700

    removed statusline from set.lua

--- lua/rayhan/set.lua
+++ lua/rayhan/set.lua
@@ -31,24 +31,6 @@ vim.opt.undodir = os.getenv(home) .. "/.vim/undodir"
 vim.opt.ruler = false
 vim.opt.showcmd = false
 
-local function get_branch()
-    local branch = string.sub(vim.fn.system("git rev-parse --abbrev-ref HEAD"), 1, -2)
-
-    if branch:match("fatal") then
-        return ""
-    end
-
-    return branch
-end
-
-local statusline = ""
-statusline = statusline .. get_branch() ~= "" and (get_branch() .. " -> ") or ""
-statusline = statusline .. "%f"
-statusline = statusline .. " %m"
-statusline = statusline .. " %r"
-
-vim.opt.statusline = statusline
-
 if is_windows then
     vim.opt.shell = "powershell.exe"
     vim.opt.shellxquote = nil

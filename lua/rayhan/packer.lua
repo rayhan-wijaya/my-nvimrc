16ed77e2919022cfc4f0890781ecbb893cae41d9
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-12 18:55:36 +0700

    removed the plugins i don't need
    ccls.nvim didn't work for me. might need to set that up myself or live
    without an lsp lol; which is bareable

--- lua/rayhan/packer.lua
+++ lua/rayhan/packer.lua
@@ -3,13 +3,10 @@ vim.cmd("packadd packer.nvim")
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
 
-  use('crispgm/telescope-heading.nvim')
   use({
 	  "nvim-telescope/telescope.nvim", tag = "0.1.1",
 	  requires = { {"nvim-lua/plenary.nvim"} }
   })
-
-  use("nvim-tree/nvim-web-devicons")
 
   use("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
 
@@ -42,15 +39,11 @@ return require("packer").startup(function(use)
   use("rayhan-wijaya/my-nvim-spectre")
   use("rayhan-wijaya/my-vim-fugitive")
 
-  use("mbbill/undotree")
-
   use("tpope/vim-surround")
   use("tpope/vim-commentary")
   use("tpope/vim-repeat")
 
   use("romainl/Apprentice")
-
-  use("ranjithshegde/ccls.nvim")
 
   use("Gavinok/vim-troff")
 

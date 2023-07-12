a1047d84273b7b55c4a12b293826d3ab96c68213
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-12 21:36:29 +0700

    reindented remap.lua

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -2,24 +2,24 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "Q", "gqip")
 
 vim.keymap.set("n", "<leader>pb", function ()
-  vim.cmd("!npm run build")
+    vim.cmd("!npm run build")
 end)
 
 vim.keymap.set("n", "<leader>s", function ()
-  local filePath = vim.fn.expand("%")
+    local filePath = vim.fn.expand("%")
 
-  vim.cmd("so")
-  print("Sourced ... " .. filePath)
+    vim.cmd("so")
+    print("Sourced ... " .. filePath)
 end)
 
 vim.keymap.set("n", "<leader>ds", function ()
-  vim.cmd(":so")
-  vim.cmd(":PackerSync")
+    vim.cmd(":so")
+    vim.cmd(":PackerSync")
 end)
 
 vim.keymap.set("n", "<leader>Q", function ()
-  vim.cmd(":%bd!")
-  vim.cmd(":wqa")
+    vim.cmd(":%bd!")
+    vim.cmd(":wqa")
 end)
 
 vim.keymap.set("n", "<leader>fw", function () vim.cmd(":w!") end)
@@ -32,25 +32,25 @@ vim.keymap.set({"v", "n"}, "<leader>P", "\"*p")
 vim.keymap.set({"v", "n"}, "<leader>d", "\"_d")
 
 vim.keymap.set("n", "<leader>ff", function ()
-  -- i'm sorry okay, i sometimes forget to save
+    -- i'm sorry okay, i sometimes forget to save
 
-  vim.cmd("w!")
+    vim.cmd("w!")
 
-  local filePath = vim.fn.expand("%:p")
-  local command = "! npx prettier --write " .. filePath
+    local filePath = vim.fn.expand("%:p")
+    local command = "! npx prettier --write " .. filePath
 
-  vim.cmd(command)
+    vim.cmd(command)
 end)
 
 vim.keymap.set("n", "<leader>T", function ()
-  local parsers = vim.fn.input("Parsers ... ")
-  vim.cmd("TSInstall " .. parsers)
+    local parsers = vim.fn.input("Parsers ... ")
+    vim.cmd("TSInstall " .. parsers)
 end)
 
 vim.keymap.set("n", "<leader>E", function ()
-  vim.cmd("Explore")
+    vim.cmd("Explore")
 end)
 
 vim.keymap.set("n", "<leader>er", function ()
-  vim.cmd("Explore .")
+    vim.cmd("Explore .")
 end)

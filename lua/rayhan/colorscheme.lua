1843a46ad9a3045b5264979d902501388c1b7393
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-12 21:33:24 +0700

    extracted it out of the function

--- lua/rayhan/colorscheme.lua
+++ lua/rayhan/colorscheme.lua
@@ -1,14 +1,11 @@
-local color_line_numbers = function ()
-  local number_color = "#5C6780"
-
-  vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function ()
-      vim.api.nvim_set_hl(0, 'LineNrAbove', { fg=number_color, bold=true })
-      vim.api.nvim_set_hl(0, 'LineNr', { fg=number_color, bold=true })
-      vim.api.nvim_set_hl(0, 'LineNrBelow', { fg=number_color, bold=true })
-    end
-  })
-end
-
 vim.cmd.colorscheme("apprentice")
-color_line_numbers()
+
+local number_color = "#5C6780"
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function ()
+        vim.api.nvim_set_hl(0, 'LineNrAbove', { fg=number_color, bold=true })
+        vim.api.nvim_set_hl(0, 'LineNr', { fg=number_color, bold=true })
+        vim.api.nvim_set_hl(0, 'LineNrBelow', { fg=number_color, bold=true })
+    end
+})

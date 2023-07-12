d1b43270d0d0c07a5bb3e41b5183b05fcfd03d77
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-07-12 21:33:43 +0700

    moved number_color inside callback function

--- lua/rayhan/colorscheme.lua
+++ lua/rayhan/colorscheme.lua
@@ -1,9 +1,9 @@
 vim.cmd.colorscheme("apprentice")
 
-local number_color = "#5C6780"
-
 vim.api.nvim_create_autocmd("BufEnter", {
     callback = function ()
+        local number_color = "#5C6780"
+
         vim.api.nvim_set_hl(0, 'LineNrAbove', { fg=number_color, bold=true })
         vim.api.nvim_set_hl(0, 'LineNr', { fg=number_color, bold=true })
         vim.api.nvim_set_hl(0, 'LineNrBelow', { fg=number_color, bold=true })

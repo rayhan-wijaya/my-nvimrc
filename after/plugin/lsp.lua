510591664a2921a37ad0502ef75895091242d0af
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-13 12:55:27 +0700

    changed "view references" to `<leader>re`

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,7 +18,7 @@ lsp.on_attach(function (_, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "<leader>rs", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>R", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>re", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "<leader>A", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
     vim.keymap.set("n", "<leader>]d", vim.diagnostic.goto_next, opts)

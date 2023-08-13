45df1db12efa8ea3d13e2a8aa10941345a072be6
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-13 12:54:34 +0700

    changed lsp mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,8 +18,8 @@ lsp.on_attach(function (_, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "<leader>rs", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>R", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>A", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
     vim.keymap.set("n", "<leader>]d", vim.diagnostic.goto_next, opts)
     vim.keymap.set("n", "<leader>[d", vim.diagnostic.goto_prev, opts)

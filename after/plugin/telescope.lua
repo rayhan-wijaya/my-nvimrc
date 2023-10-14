4f36749104037462e59371c352e51ea1482889f9
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-10-14 11:04:20 +0700

    use relative cursor layout

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,13 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
+local themes = require("telescope.themes")
 
-vim.keymap.set("n", "<C-p>", builtin.find_files)
+vim.keymap.set("n", "<C-p>", function ()
+    builtin.find_files(themes.get_cursor({
+        layout_config = {
+            width = 90,
+        },
+    }))
+end)
 vim.keymap.set("n", "<leader>gfs", builtin.git_files)
 vim.keymap.set("n", "<leader>ds", builtin.diagnostics)
 vim.keymap.set("n", "<leader>gs", function ()

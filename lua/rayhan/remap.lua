bab14fad1aa336db662a0538e19f23f5da940a69
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-11-06 15:12:49 +0700

    created "// @ts-check" insertion macro

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -43,3 +43,5 @@ vim.keymap.set("n", "<leader>er", function () vim.cmd("Explore .") end)
 
 vim.keymap.set("n", "<C-j>", function () vim.cmd("cn") end)
 vim.keymap.set("n", "<C-k>", function () vim.cmd("cp") end)
+
+vim.cmd(":let @t=\"ggO// @ts-checkogg\"")

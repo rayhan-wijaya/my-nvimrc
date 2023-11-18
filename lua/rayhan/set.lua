912d4c47c4210870a1210f3bb903702246b7e8b3
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-11-18 12:33:56 +0700

    hide "../" in netrw

--- lua/rayhan/set.lua
+++ lua/rayhan/set.lua
@@ -36,7 +36,7 @@ vim.opt.shortmess = "filnxtToFW"
 
 vim.g.netrw_banner = 0
 vim.g.netrw_bufsettings = "nu rnu nowrap"
-vim.g.netrw_list_hide = "^\\./$"
+vim.g.netrw_list_hide = "^\\./$,^\\.\\./$"
 
 vim.opt.ruler = false
 vim.opt.showcmd = false

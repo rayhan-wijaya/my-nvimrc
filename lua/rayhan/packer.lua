54e11a7874964cef734d979c117f9932ee009090
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-11-30 17:10:12 +0700

    removed lsp-zero and mason

--- lua/rayhan/packer.lua
+++ lua/rayhan/packer.lua
@@ -3,31 +3,6 @@ vim.cmd("packadd packer.nvim")
 return require("packer").startup(function(use)
     use("wbthomason/packer.nvim")
 
-    use({
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v2.x",
-        requires = {
-            {"neovim/nvim-lspconfig"},
-            {
-                "williamboman/mason.nvim",
-                run = function()
-                    pcall(function ()
-                        vim.cmd("MasonUpdate")
-                    end)
-                end,
-            },
-            {"williamboman/mason-lspconfig.nvim"},
-            {"hrsh7th/nvim-cmp"},
-            {"hrsh7th/cmp-nvim-lsp"},
-            {"L3MON4D3/LuaSnip"},
-        }
-    })
-
-    use({
-        "williamboman/mason.nvim",
-        run = ":MasonUpdate"
-    })
-
     use("rayhan-wijaya/my-vim-fugitive")
     use("rayhan-wijaya/my-vim-diff-toggle")
 

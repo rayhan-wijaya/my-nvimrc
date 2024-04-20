922113b7b719785e5f95185c969cc2a975494b0d
rayhan-wijaya <rayhanwijaya210@gmail.com>
2024-04-20 17:14:24 +0700

    corrected file types

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -17,8 +17,7 @@ local formatters = {
     -- depends on: github.com/rayhan-wijaya/prettierd-wrapper
     javascript = "! prettierd_wrapper",
     json = "! prettierd_wrapper",
-    svg = "! prettierd_wrapper",
-    typescript = "! prettierd_wrapper",
+    typescriptreact = "! prettierd_wrapper",
     astro = "! prettierd_wrapper",
     html = "! prettierd_wrapper",
     tsx = "! prettierd_wrapper",

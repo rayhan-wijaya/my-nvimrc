e2edc059191c187eb0ce53a48e76cdbbd10ffbd7
rayhan-wijaya <rayhanwijaya210@gmail.com>
2025-02-17 20:27:23 +0700

    setup cmp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -33,3 +33,17 @@ lsp.on_attach(function(_, bufnr)
 end)
 
 lsp.setup()
+
+local cmp = require("cmp")
+
+cmp.setup({
+	sources = {
+		{ name = "nvim_lsp" },
+	},
+	snippet = {
+		expand = function(args)
+			vim.snippet.expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({}),
+})

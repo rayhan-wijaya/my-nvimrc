db9394640b8175e9a79bb9ff561408c3f09c368c
rayhan-wijaya <rayhanwijaya210@gmail.com>
2025-02-18 13:36:41 +0700

    replaced single quotes

--- lua/rayhan/lazy/lsp.lua
+++ lua/rayhan/lazy/lsp.lua
@@ -42,20 +42,20 @@ return {
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					require('luasnip').lsp_expand(args.body)
+					require("luasnip").lsp_expand(args.body)
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-				['<C-y>'] = cmp.mapping.confirm({ select = true }),
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				["<C-Space>"] = function () end,
 			}),
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
 			}, {
-				{ name = 'buffer' },
+				{ name = "buffer" },
 			})
 		})
 	end,

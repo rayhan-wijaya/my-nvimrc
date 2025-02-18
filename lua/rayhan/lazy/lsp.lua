7f8ee5db3d6a80d788fdcad0b3be45fb0d0aa1d6
rayhan-wijaya <rayhanwijaya210@gmail.com>
2025-02-18 13:35:49 +0700

    configured cmp

--- lua/rayhan/lazy/lsp.lua
+++ lua/rayhan/lazy/lsp.lua
@@ -37,17 +37,26 @@ return {
 		})
 
 		local cmp = require("cmp")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 		cmp.setup({
-			sources = {
-				{ name = "nvim_lsp" },
-			},
 			snippet = {
 				expand = function(args)
-					vim.snippet.expand(args.body)
+					require('luasnip').lsp_expand(args.body)
 				end,
 			},
-			mapping = cmp.mapping.preset.insert({}),
+			mapping = cmp.mapping.preset.insert({
+				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+				['<C-y>'] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = function () end,
+			}),
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+			}, {
+				{ name = 'buffer' },
+			})
 		})
 	end,
 }

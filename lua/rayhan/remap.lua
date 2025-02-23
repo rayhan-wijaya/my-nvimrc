70fddddfa83238ffe1c06cf31c745773fb69b350
rayhan-wijaya <rayhanwijaya210@gmail.com>
2025-02-23 14:04:18 +0700

    use plus register

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
 
-vim.keymap.set({"v", "n"}, "<leader>y", "\"*y")
-vim.keymap.set({"v", "n"}, "<leader>p", "\"*p")
+vim.keymap.set({"v", "n"}, "<leader>y", "\"+y")
+vim.keymap.set({"v", "n"}, "<leader>p", "\"+p")
 vim.keymap.set({"v", "n"}, "<leader>d", "\"_d")

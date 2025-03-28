ad9883d5b27dcf9fbcaaa61ee9910b893e8e2792
rayhan-wijaya <rayhanwijaya210@gmail.com>
2025-03-28 19:02:46 +0700

    refactor statusline

--- lua/rayhan/init.lua
+++ lua/rayhan/init.lua
@@ -8,21 +8,18 @@ local function get_branch()
 	local branch_result = vim.fn.system("git branch --show-current")
 	local branch_name = string.sub(branch_result, 1, -2)
 
-	if branch_name:match("fatal") then
-		return ""
-	end
+	if branch_name:match("fatal") then return "" end
 
 	return branch_name
 end
 
 timer:start(0, 0, vim.schedule_wrap(function ()
 	local branch_name = get_branch()
+	local statusline = "%f %m %r"
 
-	local statusline = ""
-	statusline = statusline .. branch_name ~= "" and (branch_name .. " ->") or ""
-	statusline = statusline .. " %f"
-	statusline = statusline .. " %m"
-	statusline = statusline .. " %r"
+	if branch_name ~= "" then
+		statusline = string.format("%s -> %s", branch_name, statusline)
+	end
 
 	vim.opt.statusline = statusline
 end))

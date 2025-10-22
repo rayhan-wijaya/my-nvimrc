604a2fccf04023e24a3dcfddd2d8d00134eed26c
rayhan-wijaya <rayhanwijaya210@gmail.com>
2025-10-22 15:30:10 +0700

    create tabnext mapping

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -4,4 +4,5 @@ vim.keymap.set({"v", "n"}, "<leader>y", "\"+y")
 vim.keymap.set({"v", "n"}, "<leader>p", "\"+p")
 vim.keymap.set({"v", "n"}, "<leader>d", "\"_d")
 
+vim.keymap.set("n", "<C-Tab>", ":tabn")
 vim.fn.setreg("e", "_vg_")

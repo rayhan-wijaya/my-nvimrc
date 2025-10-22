caf90dcb712e7b8d52777e42e76db4f438b606a9
rayhan-wijaya <rayhanwijaya210@gmail.com>
2025-10-22 15:31:52 +0700

    fixed mapping

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -4,5 +4,5 @@ vim.keymap.set({"v", "n"}, "<leader>y", "\"+y")
 vim.keymap.set({"v", "n"}, "<leader>p", "\"+p")
 vim.keymap.set({"v", "n"}, "<leader>d", "\"_d")
 
-vim.keymap.set("n", "<C-Tab>", ":tabn")
+vim.keymap.set("n", "<C-Tab>", vim.cmd.tabn)
 vim.fn.setreg("e", "_vg_")

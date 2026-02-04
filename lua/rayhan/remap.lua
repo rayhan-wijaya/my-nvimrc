ad58e307a51498cc4125522d3e6e011d2b160e89
rayhan-wijaya <rayhanwijaya210@gmail.com>
2026-02-04 19:35:43 +0700

    telescope & update remap

--- lua/rayhan/remap.lua
+++ lua/rayhan/remap.lua
@@ -4,5 +4,6 @@ vim.keymap.set({"v", "n"}, "<leader>y", "\"+y")
 vim.keymap.set({"v", "n"}, "<leader>p", "\"+p")
 vim.keymap.set({"v", "n"}, "<leader>d", "\"_d")
 
-vim.keymap.set("n", "<C-Tab>", vim.cmd.tabn)
-vim.fn.setreg("e", "_vg_")
+vim.keymap.set("n", "<Tab>", vim.cmd.tabn)
+vim.keymap.set("n", "<A-v>", "_vg_")
+-- vim.fn.setreg("e", "_vg_")

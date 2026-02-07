8688c8d46e4d029039c161a2f968b9725690be89
rayhan-wijaya <rayhanwijaya210@gmail.com>
2026-02-07 21:07:39 +0700

    added `neoformat_enabled` property

--- lua/rayhan/lazy/format.lua
+++ lua/rayhan/lazy/format.lua
@@ -1,11 +1,13 @@
 return {
 	"sbdchd/neoformat",
 	config = function()
+		vim.g.neoformat_enabled = 1
+
 		vim.api.nvim_create_augroup("fmt", { clear = true })
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			group = "fmt",
 			pattern = "*",
-			command = "try | undojoin | Neoformat | catch /E790/ | Neoformat | endtry",
+			command = ":if g:neoformat_enabled == 1 | try | undojoin | Neoformat | catch /E790/ | Neoformat | endtry | endif",
 		})
 	end,
 }
